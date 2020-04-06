1a2f9d1402c9f3bcbc7c306b647588ab41d8f401
Joseph Griego <joseph.j.griego@gmail.com>
2020-04-05 22:03:25 -0400

    Brain dump

--- src/app.ts
+++ src/app.ts
@@ -10,8 +10,9 @@ import * as WebSocket from 'ws'
 
 let app = express();
 
-app.use('/static', express.static("assets"))
+app.use('/', express.static("assets/html"))
 app.use('/static/js', express.static("dist/ui"))
+app.use('/static/style', express.static("assets/style"))
 app.use(CookieParser());
 app.use(Session.middleware);
 
@@ -20,7 +21,12 @@ let gs = new GameServer(CounterEngine, server);
 
 var activeGame: string | null = null;
 
-app.get('/', (req, res) => {
+app.get('/api/session', (req, res) => {
+  let r = req as (typeof req & {session: Session.Session});
+  res.send(r.session.id);
+});
+
+app.get('/api/activeGame', (req, res) => {
   let r = req as (typeof req & {session: Session.Session});
   res.send(activeGame);
 });
--- src/ui.tsx
+++ src/ui.tsx
@@ -1,9 +1,20 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 
-import { Hello } from "./components/Hello.tsx";
+import { SessionProvider } from "./components/providers/SessionProvider.tsx"
+import { Home } from "./components/Home.tsx";
 
 ReactDOM.render(
-  <Hello compiler="typescript" framework="React"/>,
+  <SessionProvider>
+    <Router>
+      "hello"
+      <Switch>
+        <Route path="/">
+          <Home/>
+        </Route>
+      </Switch>
+    </Router>
+  </SessionProvider>,
   document.getElementById("example")
 );
