f05be9f785d4bf3afa45fa4686b3c3e35effd83f
Max Wang <mxawng@gmail.com>
2020-04-27 18:56:18 -0400

    hook up some callbacks to the GameClient

--- src/components/zpy/Game.tsx
+++ src/components/zpy/Game.tsx
@@ -8,6 +8,7 @@ import * as React from 'react'
 
 import { GameId } from 'server/server.ts'
 import { GameClient } from 'protocol/client.ts'
+import * as P from 'protocol/protocol.ts'
 
 import * as ZPYEngine from 'lib/zpy/engine.ts'
 
@@ -29,10 +30,30 @@ export class Game extends React.Component<Game.Props, Game.State> {
   constructor(props: Game.Props) {
     super(props);
 
-    this.state = { client: new GameClient(ZPYEngine, props.path, props.id) };
+    const client: Client = new GameClient(ZPYEngine, props.path, props.id);
+    client.onClose  = this.onClose  = this.onClose.bind(this);
+    client.onReset  = this.onReset  = this.onReset.bind(this);
+    client.onUpdate = this.onUpdate = this.onUpdate.bind(this);
+
+    this.state = {client};
+  }
+
+  onClose(client: Client) {
+    this.setState({client: null});
+  }
+  onReset(client: Client) {
+    this.setState({client});
+  }
+  onUpdate(client: Client, effect: ZPYEngine.Effect | P.ProtocolAction) {
+    this.setState({client});
   }
 
   render() {
+    if (this.state.client === null) {
+      return <div className="done">
+        the game has ended
+      </div>;
+    }
     return <Board/>;
   }
 }
@@ -45,7 +66,7 @@ export type Props = {
 };
 
 export type State = {
-  client: Client;
+  client: null | Client;
 };
 
 }
