316e8486142667ac1a03aed3444f38b775ac5980
Max Wang <mxawng@gmail.com>
2020-05-03 11:18:14 -0400

    add a comment

--- src/components/zpy/PlayArea.tsx
+++ src/components/zpy/PlayArea.tsx
@@ -1275,6 +1275,7 @@ export type State = {
     pile: CardBase[];
   };
 
+  // selected card is in the friend selector
   fr_select: Record<string, [CardBase, number]>[];
 
   // is there an action pending?
